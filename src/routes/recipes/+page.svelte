cadd9151dd2ae6f45afbb9c1f8fef6b75bd6362c
eallenOP <elise.allen@op.ac.nz>
2025-10-28 10:59:32 +1300

    Adjust layout for first breakpoint: remove cuisine column

--- src/routes/recipes/+page.svelte
+++ src/routes/recipes/+page.svelte
@@ -1,44 +1,51 @@
 <script>
-    let { data } = $props();
-    console.log(data)
+  let { data } = $props();
+  console.log(data);
 </script>
 
 <table>
-    <thead>
-        <tr>
-            <th>Recipe Name</th>
-            <th>Prep time</th>
-            <th>Difficulty</th>
-            <th>Servings</th>
-            <th>Cuisine</th>
-            <th>Calories</th>
-        </tr>
-    </thead>
-    <tbody>
-        {#each data.recipes.recipes as recipe}
-            <tr>
-                <td>{recipe.name}</td>
-                <td>{recipe.prepTimeMinutes}</td>
-                <td>{recipe.difficulty}</td>
-                <td>{recipe.servings}</td>
-                <td>{recipe.cuisine}</td>
-                <td>{recipe.caloriesPerServing}</td>
-            </tr>
-        {/each}
-    </tbody>
+  <thead>
+    <tr>
+      <th>Recipe Name</th>
+      <th>Prep time</th>
+      <th>Difficulty</th>
+      <th>Servings</th>
+      <th>Cuisine</th>
+      <th>Calories</th>
+    </tr>
+  </thead>
+  <tbody>
+    {#each data.recipes.recipes as recipe}
+      <tr>
+        <td>{recipe.name}</td>
+        <td>{recipe.prepTimeMinutes}</td>
+        <td>{recipe.difficulty}</td>
+        <td>{recipe.servings}</td>
+        <td>{recipe.cuisine}</td>
+        <td>{recipe.caloriesPerServing}</td>
+      </tr>
+    {/each}
+  </tbody>
 </table>
 
 <style>
-    table {
-        table-layout: fixed;
-        width: 90%;
-        margin: 10px auto;
-        border-collapse: collapse;
+  table {
+    table-layout: fixed;
+    width: 90%;
+    margin: 10px auto;
+    border-collapse: collapse;
+  }
+
+  th,
+  td {
+    border: 1px solid #999;
+    padding: 0.6em;
+  }
+
+  @media (max-width: 768px) {
+    tr th:nth-child(5),
+    tr td:nth-child(5) {
+      display: none;
     }
-    
-    th,
-    td {
-        border: 1px solid #999;
-        padding: 0.6em;
-    }
+  }
 </style>
