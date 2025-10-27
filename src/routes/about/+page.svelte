dfb5b41a97dcf222f299acb5a13d55eed6198b3e
eallenOP <elise.allen@op.ac.nz>
2025-10-28 11:21:25 +1300

    Add about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,3 @@
+<h2>About this site</h2>
+<p>This is a demonstration of making a responsive table design in SvelteKit.</p>
+<p>We are fetching a list of recipes from dummyjson.com and displaying them in a table view with plenty of columns showing on a large screen. Then as the screen size gets smaller, we are removing the least-needed columns to fix the layout.</p>
