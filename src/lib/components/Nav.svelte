0e6b6ebeb2a4c67fb797de1aedcdbd5ee891fdd1
eallenOP <elise.allen@op.ac.nz>
2025-10-28 11:03:57 +1300

    Add a basic nav

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,15 @@
+<nav>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/recipes">Recipes</a></li>
+        <li><a href="/contact">Contact us</a></li>
+    </ul>
+</nav>
+
+<style>
+    ul {
+        list-style: none;
+        display: flex;
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
-
+	import Nav from '$lib/components/Nav.svelte';
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
+<Nav />
 
 {@render children?.()}
 
