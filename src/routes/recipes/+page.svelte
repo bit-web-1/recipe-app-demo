1d2bfbed92e15343a33e7cc6eaffeed332205b1b
eallenOP <elise.allen@op.ac.nz>
2025-10-28 10:46:51 +1300

    Display recipe data as a table

--- src/routes/recipes/+page.svelte
+++ src/routes/recipes/+page.svelte
@@ -2,3 +2,28 @@
     let { data } = $props();
     console.log(data)
 </script>
+
+<table>
+    <thead>
+        <tr>
+            <td>Recipe Name</td>
+            <td>Prep time</td>
+            <td>Difficulty</td>
+            <td>Servings</td>
+            <td>Cuisine</td>
+            <td>Calories</td>
+        </tr>
+    </thead>
+    <tbody>
+        {#each data.recipes.recipes as recipe}
+            <tr>
+                <td>{recipe.name}</td>
+                <td>{recipe.prepTimeMinutes}</td>
+                <td>{recipe.difficulty}</td>
+                <td>{recipe.servings}</td>
+                <td>{recipe.cuisine}</td>
+                <td>{recipe.caloriesPerServing}</td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
