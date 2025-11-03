7d3d521e2d0658214e823a140e81b739c467e26e
eallenOP <elise.allen@op.ac.nz>
2025-11-04 10:52:52 +1300

    Add links to about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,3 @@
 <h2>About this site</h2>
-<p>This is a demonstration of making a responsive table design in SvelteKit.</p>
-<p>We are fetching a list of recipes from dummyjson.com and displaying them in a table view with plenty of columns showing on a large screen. Then as the screen size gets smaller, we are removing the least-needed columns to fix the layout.</p>
+<p>This is a demonstration of making a <a href="/recipes">responsive table design</a> in SvelteKit.</p>
+<p>We are fetching a list of recipes from <a href="https://dummyjson.com/docs/recipes">dummyjson.com</a> and displaying them in a table view with plenty of columns showing on a large screen. Then as the screen size gets smaller, we are removing the least-needed columns to fix the layout.</p>
